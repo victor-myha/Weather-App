64a6762b651555181d77919337d53dba9eef9c38
nationalcodecare <victormyhaletsky@gmail.com>
2021-07-01 17:03:26 +0300

    sproba 2

--- src/api/api.js
+++ src/api/api.js
@@ -3,13 +3,13 @@ import * as axios from 'axios';
 const apiKey = 'NccctTZf87uJfckbVI2iZjGFG0eKctx8';
 export const API = {
     getCities(cityName) {
-        return axios.get(`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${cityName}`)
+        return axios.get(`https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${cityName}`)
         .then(response =>{
              return response.data ;
         });
     },
     getWeather(LocationKey){
-        return axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${LocationKey}?apikey=${apiKey}`)
+        return axios.get(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/${LocationKey}?apikey=${apiKey}`)
         .then(response =>{
              return response.data.DailyForecasts ;
         });
